fd7a989cb2dc49bbc20057b4e418d46471e58e39
Chat-Aiano1 <mcrypto949@gmail.com>
2024-10-20 05:20:39 +0300

    First commit

---
+++ script.sh
@@ -0,0 +1,15 @@
+git init
+git config --global user.email "mcrypto949@gmail.com"
+git config --global user.name "Chat-Aiano1"
+git add README.md
+gut add main.cpp
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Chat-Aiano1/cpp_project
+git push -u origin main
+
+git remote add origin https://github.com/Chat-Aiano1/cpp_project
+git branch -M main
+git push -u origin main
+
+
